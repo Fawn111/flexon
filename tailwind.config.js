fba2023b92ed33a2e108fe5db0f7a2db40e28135
Muhammad Moiz Riaz <91956008+Fawn111@users.noreply.github.com>
2024-09-13 18:43:15 +0500

    flexon commit

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      animation: {
+        "loop-scroll": "loop-scroll 50s linear infinite",
+      },
+      keyframes: {
+        "loop-scroll": {
+          from: { transform: "translateX(0)" },
+          to: { transform: "translateX(-100%)"},
+        },
+      },
+      fontFamily: {
+        Manrope: ["Manrope", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+}
+
